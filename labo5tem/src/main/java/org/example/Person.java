e8bb7bbc98522d8d9677919ed53b9b530c5108cc
FechitaDiandra <161494390+FechitaDiandra@users.noreply.github.com>
2024-04-05 17:52:29 +0300

    Update Person.java

--- labo5tem/src/main/java/org/example/Person.java
+++ labo5tem/src/main/java/org/example/Person.java
@@ -1,5 +1,9 @@
 package org.example;
 
-record Person(int id, String name) {
+/**
+ * The Person class represents a person with an ID and a name.
+ * This class is a record, which is an immutable data carrier.
+ */
+public record Person(int id, String name) {
+    
 }
-
