3a6b293135e1db7c06ce26627d96e22ebe0fb2b6
FechitaDiandra <161494390+FechitaDiandra@users.noreply.github.com>
2024-04-05 17:49:36 +0300

    Update Document.java

--- labo5tem/src/main/java/org/example/Document.java
+++ labo5tem/src/main/java/org/example/Document.java
@@ -1,4 +1,9 @@
 package org.example;
 
+/**
+ * The Document class represents a document with a name.
+ * This class is a record, which is an immutable data carrier.
+ */
 public record Document(String name) {
+
 }
