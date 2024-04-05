445b1d44fcb681ef0aba5b10e9757e1ef89e2520
FechitaDiandra <161494390+FechitaDiandra@users.noreply.github.com>
2024-04-05 17:59:51 +0300

    Update View.java

--- labo5tem/src/main/java/org/example/View.java
+++ labo5tem/src/main/java/org/example/View.java
@@ -5,14 +5,30 @@ import org.exceptions.ShellException;
 import java.awt.*;
 import java.io.File;
 
+/**
+ * The View class represents a command to view a document.
+ * It implements the Command interface, allowing it to be executed as a command.
+ */
 public class View implements Command {
 
-    String command;
+    /** The command string provided to the View object. */
+    private String command;
 
-    View(String command) {
+    /**
+     * Constructs a View command with the given command string.
+     *
+     * @param command the command string representing the file path to view
+     */
+    public View(String command) {
         this.command = command;
     }
 
+    /**
+     * Executes the view command by opening the specified document.
+     * If the file path is not provided or the file does not exist, appropriate messages are printed.
+     *
+     * @throws ShellException if an error occurs while trying to view the document
+     */
     @Override
     public void execute() {
         try {
@@ -34,5 +50,4 @@ public class View implements Command {
             throw new ShellException("Error trying to view a document: " + e.getMessage());
         }
     }
-
 }
