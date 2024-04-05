ce09b67aac86c43bd9d84dc7fab8494817ca65d1
FechitaDiandra <161494390+FechitaDiandra@users.noreply.github.com>
2024-04-05 17:09:51 +0300

    Add files via upload

---
+++ labo5tem/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Repository repository = new Repository("C:\\Users\\User\\Desktop\\Java\\labo5tem");
+        repository.displayRepositoryContent();
+    }
+}
