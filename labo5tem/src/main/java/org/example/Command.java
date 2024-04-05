7b2e36f8aaff0f1dca066d0ec901362b9b13ddf6
FechitaDiandra <161494390+FechitaDiandra@users.noreply.github.com>
2024-04-05 17:48:14 +0300

    Update Command.java

--- labo5tem/src/main/java/org/example/Command.java
+++ labo5tem/src/main/java/org/example/Command.java
@@ -2,7 +2,17 @@ package org.example;
 
 import java.io.IOException;
 
+/**
+ * The Command interface represents an action that can be executed.
+ * Implementations of this interface should provide a method to execute the command.
+ */
 public interface Command {
+    
+    /**
+     * Executes the command.
+     * 
+     * @throws IOException if an I/O error occurs while executing the command
+     */
     void execute() throws IOException;
 
 }
